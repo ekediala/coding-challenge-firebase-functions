75bb32f658c202ef3f465010451fe8fd259c9ca3
Eke Enyinnaya Diala <ekeenyinnaya@konetweb.com>
2019-11-04 19:12:46 +0100

    added express and cors

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,37 @@
 const functions = require('firebase-functions');
 const uuidv5 = require('uuid/v5');
 const uuidv4 = require('uuid/v4');
+const admin = require('firebase-admin');
 
-exports.addUUid = functions.database
+admin.initializeApp(functions.config().firebase);
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+app.post('/create', (req, res) => {
+  const userRef = admin.database().ref('/users');
+  const uniqueIdentifier = uuidv4();
+  const userData = req.body;
+  const key = `${userData.firstName}-${userData.lastName}-${uniqueIdentifier}`;
+  userData.key = key;
+  userRef.push(userData);
+  res.end();
+});
+
+// Expose Express API as a single Cloud Function:
+exports.createUser = functions.https.onRequest(app);
+
+exports.addUuid = functions.database
   .ref('/users/{userId}')
   .onCreate(snapshot => {
     const userRef = snapshot.ref;
-    const userData = snapshot.val();
     const uniqueIdentifier = uuidv4();
-    const uuidName = `${userData.firstName} ${userData.lastName} ${uniqueIdentifier}`;
-    const userId = uuidv5(uuidName, uuidv5.URL);
-    return userRef.update({
-      userId,
-    });
+    const userData = snapshot.val();
+    const name = `${userData.firstName} ${userData.lastName} ${uniqueIdentifier}`;
+    const userId = uuidv5(name, uuidv5.DNS);
+    return userRef.update({ userId });
   });
